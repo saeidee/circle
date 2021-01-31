e01168e9365603011334ac12776bd53f7fa6caa9
Saeid Kanishka Saeidee <sk.saeidee@yahoo.com>
2021-02-01 00:18:11 +0300

    seeder folder added

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+/**
+ * Class DatabaseSeeder
+ */
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * @return void
+     */
+    public function run(): void
+    {
+
+    }
+}
